3f2557063285c2994bb15b2bdcd6c3b2de8b010e
Kartagis <tolga@ozses.net>
2019-03-22 19:54:03 +0000

    an attempt to make a text formatter plugin

---
+++ src/Plugin/Field/FieldFormatter/AmazeeTextFormatter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\amazee\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ *
+ */
+
+ class AmazeeTextFormatter extends FormatterBase
+ {
+   public function foo($bar)
+   {
+     return "1";
+   }
+ }
