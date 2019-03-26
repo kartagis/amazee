5b24270e05fc69e6f7f83b1adc842b59c934a5ed
Kartagis <tolga@ozses.net>
2019-03-26 11:31:08 +0000

    deleted the fieldformatter, added a text formatter

---
+++ src/Plugin/Filter/CalculatorFilter.php
@@ -0,0 +1,26 @@
+<?php
+namespace Drupal\amazee\Plugin\Filter;
+
+use \Drupal\filter\Plugin\FilterBase;
+use \Drupal\filter\FilterProcessResult;
+use jlawrence\eos\Parser;
+
+/**
+ * @Filter(
+ *   id = "filter_amazee",
+ *   title = @Translation("My custom text filter"),
+ *   description = @Translation("Perform calculations"),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ * )
+ */
+class CalculatorFilter extends FilterBase
+{
+  protected $eq;
+  protected $vars;
+
+  function process($text, $langcode)
+  {
+    $new_text = str_replace($text, Parser::solve($eq, $vars), $text);
+    return new FilterProcessResult($new_text);
+  }
+}
